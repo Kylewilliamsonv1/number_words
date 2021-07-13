436640afd391364184597c1153e9873c4c32470f
Kylewilliamsonv1 <84644678+Kylewilliamsonv1@users.noreply.github.com>
2021-07-13 14:40:17 -0700

    test- grab hash value
    co-authored-by: James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>

--- lib/number_words.rb
+++ lib/number_words.rb
@@ -3,12 +3,38 @@ class NumberWords
     @num = number
   end
   def numbers_to_words(num)
-    number_words = Hash.new()
-    word = 'one'
-    number_words.store(num, word)
-    number_words.fetch(num)
+    ones_hash = { 1 => 'one', 2 => 'two', 3 => 'three', 4 => 'four', 5 => 'five', 6 => 'six', 7 => 'seven', 8 => 'eight', 9 => 'nine', 10 => 'ten', 11 => 'eleven', 12 => 'twelve', 13 => 'thirteen', 14 => 'fourteen', 15 => 'fifteen', 16 => 'sixteen', 17 => 'sixteen', 18 => 'eighteen', 19 => 'nineteen', 20 =}
+    tens_hash = { 2=> 'twenty', 3 => 'thirty', 4 => 'forty', 5 => 'fifty', 6 => 'sixty', 7 => 'seventy', 8 => 'eighty', 9 => 'ninety'}
+    hundreds_hash = { 1 => 'one hundred', 2 => 'two hundred', 3 => 'three hundred', 4 => 'four hundred', 5 => 'five hundred', 6 => 'six hundred', 7 => 'seven hundred', 8 => 'eight hundred', 9 => 'nine hundred'}
+    thousands_hash = { 1 => 'one thousand', 2 => 'two thousand', 3 => 'three thousand', 4 => 'four thousand', 5 => 'five thousand', 6 => 'six thousand', 7 => 'seven thousand', 8 => 'eight thousand', 9 => 'nine thousand'}
+    array = @num.divmod(1000)
+    length = array.length
+    if length === 1
+      ones_hash.fetch(@num)
+    # number_words.fetch(@num)
+    if length === 
+    end
+    return ones_hash.fetch(@num)
   end
 end
 
+# x = 876885
+# x.to_s.scan(/.../).map {|e| e.to_i }
+# => [876, 885]
+# x.divmod(1000)
 
-# store hash, then fetch hash that will take key as the argument and return value
+# 386
+# three hundred eighty six
+
+# array = number.split('')
+# array.length
+# if array.length = 3
+# num1 = hundreds_hash.fetch(@num)
+# array[0].push hundreds
+
+#   # 1,000,000,000,000
+
+# If number has three digits, if first digit is 3, use hundreds hash, if second digit is two, use tens hash, if third digit is 1, use ones hash
+
+# #1 is 'one', 2 is 'two'...
+# # store hash, then fetch hash that will take key as the argument and return value
--- spec/number_words_spec.rb
+++ spec/number_words_spec.rb
@@ -3,7 +3,11 @@ require 'number_words'
 
 describe('#numbers_to_words') do
   it("should return the word equivalent of 1") do
-    num = NumberWords.new(1)
-  expect(num.numbers_to_words(num)).to(eq('one'))
+    num = NumberWords.new(2)
+  expect(num.numbers_to_words(num)).to(eq('two'))
+  end
+  it("should return the word equivalent of numbers from 1 to 9") do
+    num = NumberWords.new(9)
+  expect(num.numbers_to_words(num)).to(eq('nine'))
   end
 end
